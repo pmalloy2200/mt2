1d8b60ead3c9f90aa8b7cf5a1190c83b245e1d6c
pmalloy2200 <patrick.malloy@uconn.edu>
2014-12-14 04:46:29 -0500

    fixed .gitignore, readability and calculations

--- white-dwarf-eqs.c
+++ white-dwarf-eqs.c
@@ -7,32 +7,32 @@
 
 #include <gsl/gsl_errno.h>
 
-int dwarf_eqs (double r, const double y[], double f[], void *params)
+int
+dwarf_eqs (double r, const double y[], double f[], void *params)
 {
     double rho_c = *(double *) params;
-    
+
     double m = y[0];
     double rho = y[1];
-    
+
     if (rho > 0)
     {
-        double p = pow(rho, 2./3.);
-        double gamma = p/(3 * sqrt(1. + p));
+        double p = pow (rho, 2. / 3.);
+        double gamma = p / (3 * sqrt (1. + p));
         f[0] = rho * r * r;
         if (r > 1.e-6)
         {
-            f[1] = -m * rho/(gamma * r * r);
+            f[1] = -m * rho / (gamma * r * r);
         }
         else
         {
-            f[1] = -rho_c/3. * r * rho/gamma;
+            f[1] = -rho_c / 3. * r * rho / gamma;
         }
     }
     else
     {
-    f[0] = 0.;
-    f[1] = 0.;
+        f[0] = 0.;
+        f[1] = 0.;
     }
-  return GSL_SUCCESS;
+    return GSL_SUCCESS;
 }
-        
--- white-dwarf-iterate.c
+++ white-dwarf-iterate.c
@@ -21,59 +21,59 @@
 
 int dwarf_eqs (double r, const double y[], double f[], void *params);
 
-int main (void) 
+int
+main (void)
 {
-  size_t neqs = 2;          /* number of equations */
-  double eps_abs = 1.e-8, 
-    eps_rel = 0.;           /* desired precision */
-  double stepsize = 1e-6;   /* initial integration step */
-  double rho_c;
-  double r = 1.e-3, r1 = 50.; /* time interval */
-  int status;
-  
-   for (rho_c = 0.1; rho_c <= 1000000; rho_c*=1.2)
-  {
-  /* 
-   * Initial conditions 
-   */
-  double y[2] = {0, rho_c};
-  
-  /*
-   * Explicit embedded Runge-Kutta-Fehlberg (4,5) method. 
-   * This method is a good general-purpose integrator.
-   */
-  gsl_odeiv2_step    *s = gsl_odeiv2_step_alloc 
-                            (gsl_odeiv2_step_rkf45, neqs);
-  gsl_odeiv2_control *c = gsl_odeiv2_control_y_new (eps_abs, 
-						    eps_rel);
-  gsl_odeiv2_evolve  *e = gsl_odeiv2_evolve_alloc (neqs);
-    
-  gsl_odeiv2_system sys = {dwarf_eqs, NULL, neqs, &rho_c};
-    
-  /*
-   * Evolution loop 
-   */
-   
- 
-      
-      while ( (r < r1) && (y[1] > 0))
-      {
-        status = gsl_odeiv2_evolve_apply (e, c, s, &sys, &r,
-                                          r1, &stepsize, y);
+    size_t neqs = 2;            /* number of equations */
+    double eps_abs = 1.e-8, eps_rel = 0.;       /* desired precision */
+    double stepsize = 1e-6;     /* initial integration step */
+    double rho_c;
+    double r = 1.e-3, r1 = 50.; /* time interval */
+    int status;
 
-        if (status != GSL_SUCCESS) {
-          printf ("Troubles: % .5e  % .5e  % .5e\n",r, y[0], y[1]);
-          break;
+    for (rho_c = 0.1; rho_c <= 1000000; rho_c *= 1.2)
+    {
+        /* 
+         * Initial conditions 
+         */
+        double y[2] = { 0, rho_c };
+
+        /*
+         * Explicit embedded Runge-Kutta-Fehlberg (4,5) method. 
+         * This method is a good general-purpose integrator.
+         */
+        gsl_odeiv2_step *s = gsl_odeiv2_step_alloc
+            (gsl_odeiv2_step_rkf45, neqs);
+        gsl_odeiv2_control *c = gsl_odeiv2_control_y_new (eps_abs,
+                                                          eps_rel);
+        gsl_odeiv2_evolve *e = gsl_odeiv2_evolve_alloc (neqs);
+
+        gsl_odeiv2_system sys = { dwarf_eqs, NULL, neqs, &rho_c };
+
+        /*
+         * Evolution loop 
+         */
+
+        r = 0;
+
+        while ((r < r1) && (y[1] > 0))
+        {
+            status = gsl_odeiv2_evolve_apply (e, c, s, &sys, &r,
+                                              r1, &stepsize, y);
+
+            if (status != GSL_SUCCESS)
+            {
+                printf ("Troubles: % .5e  % .5e  % .5e\n", r, y[0], y[1]);
+                break;
+            }
+
+            // printf ("% .5e  % .5e  % .5e\n",           
+            //        r, y[0], y[1]);
         }
-
-        // printf ("% .5e  % .5e  % .5e\n", 	      
-        //        r, y[0], y[1]);
-      }
-        printf ("% .5e  % .5e %  5e\n", 	      
-                r, y[0], rho_c);
-  gsl_odeiv2_evolve_free (e);
-  gsl_odeiv2_control_free (c);
-  gsl_odeiv2_step_free (s);
+        printf ("% .5e  % .5e %  5e\n", r, y[0], rho_c);
+        gsl_odeiv2_evolve_free (e);
+        gsl_odeiv2_control_free (c);
+        gsl_odeiv2_step_free (s);
     }
-  return 0;
+    return 0;
 }
